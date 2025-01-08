72517156eb9aa5da112f810e5f52d0cf35bf35ec
Chris Trombley <chris.trombley@hashicorp.com>
2025-01-08 14:02:27 -0800

    fix issues

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,12 +1,12 @@
 output "vpc_id" {
-  value = component.vpc.vpc_id.hex
+  value = random.random_id.vpc_id.hex
 }
 
 output "subnet_private_id" {
-  value = component.vpc.subnet_private_id.hex
+  value = random.random_id.subnet_private_id.hex
 }
 
 output "subnet_public_id" {
-  value = component.vpc.subnet_public_id.hex
+  value = random.random_id.subnet_public_id.hex
 }
 
