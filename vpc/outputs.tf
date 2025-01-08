859e08cb592b7194fb8d300124d41feb2974620f
Chris Trombley <chris.trombley@hashicorp.com>
2025-01-08 14:03:40 -0800

    fix issues

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,12 +1,12 @@
 output "vpc_id" {
-  value = random.random_id.vpc_id.hex
+  value = random_id.vpc_id.hex
 }
 
 output "subnet_private_id" {
-  value = random.random_id.subnet_private_id.hex
+  value = random_id.subnet_private_id.hex
 }
 
 output "subnet_public_id" {
-  value = random.random_id.subnet_public_id.hex
+  value = random_id.subnet_public_id.hex
 }
 
